0729dd938f7fddf58efc377dfa4c6e3b13cf4c8d
seongho <damadu01@gmail.com>
2023-08-19 07:51:30 +0900

    first commit

---
+++ components/nav/Nav.jsx
@@ -0,0 +1,43 @@
+'use client'
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavBar() {
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Took-Peak</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+          <NavDropdown title="What is Took-Peak" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.3">What is Took-Peak</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">History</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Notice</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Message Board" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">자유게시판</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">협업게시판</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">포트폴리오 게시판</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">질문게시판</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Search" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">학원 검색</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="About Dev" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">개발 팁</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">개발 기술</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">CS</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">개발 면접</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
