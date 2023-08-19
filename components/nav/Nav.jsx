a9892af8a5aa711addf166cd83be514d286ee147
seongho <damadu01@gmail.com>
2023-08-19 09:09:43 +0900

    nav

--- components/nav/Nav.jsx
+++ components/nav/Nav.jsx
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
 
 function NavBar() {
   return (
@@ -35,6 +36,7 @@ function NavBar() {
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
+        <Button variant="primary" className='loginBtn'>Login</Button>
       </Container>
     </Navbar>
   );
