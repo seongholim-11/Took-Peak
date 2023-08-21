51734ad3133d9241d0d1d91eff06e653eda5799f
seongho <damadu01@gmail.com>
2023-08-21 15:02:08 +0900

    nav 수정

--- components/nav/Nav.jsx
+++ components/nav/Nav.jsx
@@ -6,6 +6,8 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 
+import './nav.scss'
+
 function NavBar() {
   return (
     <Navbar expand="lg">
@@ -19,14 +21,14 @@ function NavBar() {
               <NavDropdown.Item href="#action/3.1">History</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Notice</NavDropdown.Item>
             </NavDropdown>
+            <NavDropdown title="Search" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">학원 검색</NavDropdown.Item>
+            </NavDropdown>
             <NavDropdown title="Message Board" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">자유게시판</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.1">협업게시판</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.1">포트폴리오 게시판</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.1">질문게시판</NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown title="Search" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">학원 검색</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="About Dev" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">개발 팁</NavDropdown.Item>
@@ -34,9 +36,11 @@ function NavBar() {
               <NavDropdown.Item href="#action/3.1">CS</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.1">개발 면접</NavDropdown.Item>
             </NavDropdown>
+            <NavDropdown.Divider />
+            <Nav.Link href="#features" className='signinup'>Sign Up</Nav.Link>
+            <Nav.Link href="#features" className='signinup'>Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
-        <Button variant="primary" className='loginBtn'>Login</Button>
       </Container>
     </Navbar>
   );
