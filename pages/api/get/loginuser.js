c35b356a842c78494ece9fc17a12310fa5b2f6f7
seongho <damadu01@gmail.com>
2023-08-22 16:29:49 +0900

    user 프로필 로딩중 추가

--- pages/api/get/loginuser.js
+++ pages/api/get/loginuser.js
@@ -1,16 +1,26 @@
 import { connectDB } from "@/util/database";
-
-import { authOptions } from "@/pages/api/auth/[...nextauth].js"
-import { getServerSession } from "next-auth"
+import { authOptions } from "@/pages/api/auth/[...nextauth].js";
+import { getServerSession } from "next-auth";
 
 export default async function handler(req, res) {
-    let session = await getServerSession(req, res, authOptions)
-    let userEmail = session.user.email
+    let session = await getServerSession(req, res, authOptions);
+    if (session !== null) {
+        const userEmail = session.user.email;
 
-    if (req.method == "GET") {
-        const client = await connectDB;
-        const db = client.db("forum");
-        let result = await db.collection("user_cred").findOne({email:userEmail})
-        return res.status(200).json(result);
+        if (req.method === "GET") {
+            try {
+                const db = (await connectDB).db("forum");
+                let result = await db
+                    .collection("user_cred")
+                    .findOne({ email: userEmail });
+
+                return res.status(200).json(result);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+                return res.status(500).json({ error: "Internal server error" });
+            }
+        } else {
+            return res.status(405).json({ error: "Method not allowed" });
+        }
     }
 }
