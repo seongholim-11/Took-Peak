72103b9a57a567fbe5f35e4b63c1cb9f1578a03a
seongho <damadu01@gmail.com>
2023-08-25 00:32:51 +0900

    카카오 지도 추가

---
+++ components/main/search/Map.jsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+
+function Map() {
+  useEffect(() => {
+    const mapScript = document.createElement('script');
+
+    mapScript.async = true;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&autoload=false`;
+
+    document.head.appendChild(mapScript);
+
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const mapContainer = document.getElementById('map');
+        const mapOption = {
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+          level: 3, // 지도의 확대 레벨
+        };
+        new window.kakao.maps.Map(mapContainer, mapOption);
+      });
+    };
+    mapScript.addEventListener('load', onLoadKakaoMap);
+  }, []);
+
+  return (
+      <div id="map" style={{width: '1000px', height: '1000px'}}></div>
+  );
+}
+
+export default Map;
