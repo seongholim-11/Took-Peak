5db5a837848ace9beb3bdc0c3f8d65108b067f0e
seongho <damadu01@gmail.com>
2023-08-25 17:33:47 +0900

    addr undefined 오류

--- components/main/search/Map.jsx
+++ components/main/search/Map.jsx
@@ -1,32 +1,44 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-function Map() {
-  useEffect(() => {
-    const mapScript = document.createElement('script');
+function Map({ addr1 }) {
 
-    mapScript.async = true;
-    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&autoload=false`;
+  
+    useEffect(() => {
+        const mapScript = document.createElement("script");
 
-    document.head.appendChild(mapScript);
+        mapScript.async = true;
+        mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&libraries=services&autoload=false`;
 
-    const onLoadKakaoMap = () => {
-      window.kakao.maps.load(() => {
-        const mapContainer = document.getElementById('map');
-        const mapOption = {
-          center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-          level: 3, // 지도의 확대 레벨
+        document.head.appendChild(mapScript);
+
+        const onLoadKakaoMap = () => {
+            window.kakao.maps.load(() => {
+                const mapContainer = document.getElementById("map");
+                const mapOption = {
+                    center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+                    level: 3, // 지도의 확대 레벨
+                };
+                new window.kakao.maps.Map(mapContainer, mapOption);
+
+                var geocoder = new window.kakao.maps.services.Geocoder();
+
+                var callback = function (result, status) {
+                    if (status === window.kakao.maps.services.Status.OK) {
+                        console.log(result);
+                    }
+                };
+
+                if (addr1) {
+                    geocoder.addressSearch(`${addr1}`, callback);
+                }
+            });
         };
-        new window.kakao.maps.Map(mapContainer, mapOption);
-      });
-    };
-    mapScript.addEventListener('load', onLoadKakaoMap);
-  }, []);
+        mapScript.addEventListener("load", onLoadKakaoMap);
+    }, []);
 
-  return (
-      <div id="map" style={{width: '1000px', height: '1000px'}}></div>
-  );
+    return <div id="map" style={{ width: "1000px", height: "1000px" }}></div>;
 }
 
 export default Map;
