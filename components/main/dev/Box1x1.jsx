f60ff2f9394b830409e133482358a942067c7a82
seongho <damadu01@gmail.com>
2023-08-29 16:13:33 +0900

    메인페이지 dev ui 완성

--- components/main/dev/Box1x1.jsx
+++ components/main/dev/Box1x1.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
-export default function Box1x1() {
-  return (
-    <div>
-        
-    </div>
-  )
+export default function Box1x1({ item, idx }) {
+    return (
+        <div className={`box1 nth${idx}`}>
+            <div className="box1Img">
+                <img src={item.img} alt={item.img} />
+            </div>
+            <div className="box1Text">
+                <div className="box1Title">{item.title}</div>
+                <div className="box1Content">{item.content}</div>
+            </div>
+            <div className="boxTriangle"></div>
+        </div>
+    );
 }
