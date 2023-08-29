b375b5c37fa73623672e6e304f686e54121e04e7
seongho <damadu01@gmail.com>
2023-08-29 21:07:10 +0900

    img 이미지를 선택해주세요 기본 이미지 만들기, img ui 만들기

--- components/board/write/WriteForm.jsx
+++ components/board/write/WriteForm.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, {useState} from "react";
 
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
@@ -9,6 +9,7 @@ import Button from "react-bootstrap/Button";
 import "./write.scss";
 
 function WriteForm() {
+    const [src, setSrc] = useState("");
     return (
         <Container>
             <Form>
@@ -27,7 +28,61 @@ function WriteForm() {
                         <option value="qna">질문게시판</option>
                     </Form.Select>
                 </Form.Group>
+                <Form.Group>
+                    <Form.Group controlId="formFile">
+                        <Form.Label>사진을 첨부해주세요</Form.Label>
+                        <Form.Control
+                            type="file"
+                            id="board"
+                            /* 서버로 데이터를 전송할 때 사용 */
+                            name="boardimage"
+                            /* 이미지 파일만 선택 가능 */
+                            accept="image/*"
+                            onChange={async (e) => {
+                                /* 내용이 1개인 배열 */
+                                let file = e.target.files[0];
+                                /* URI로 데이터를 전달하기 위해서 문자열을 인코딩 */
+                                let filename = encodeURIComponent(file.name);
+                                /* 서버에 선택한 파일 이름을 전달하여 업로드를 위한 필수 정보를 요청 */
+                                let res = await fetch(
+                                    "/api/post/board?file=" + filename
+                                );
+                                /* 서버 응답을 JSON 형태로 파싱하여 필요한 정보를 추출 */
+                                res = await res.json();
 
+                                //S3 업로드
+                                /* new FormData() => 폼을 쉽게 보내도록 도와주는 객체 */
+                                const formData = new FormData();
+
+                                /* 
+                                    필수 필드와 선택한 파일을 FormData에 추가 
+                                    Object.entries() // 모든 프로퍼티와 값을 배열로 반환함
+                                    */
+                                Object.entries({
+                                    ...res.fields,
+                                    file,
+                                }).forEach(([key, value]) => {
+                                    formData.append(key, value);
+                                });
+
+                                /* FormData를 서버 URL로 POST 요청하여 파일을 S3에 업로드 */
+                                let result = await fetch(res.url, {
+                                    method: "POST",
+                                    body: formData,
+                                });
+                                console.log(result);
+
+                                if (result.ok) {
+                                    /* 이미지의 URL을 업데이트 */
+                                    setSrc(result.url + "/" + filename);
+                                } else {
+                                    console.log("실패");
+                                }
+                            }}
+                        />
+                    </Form.Group>
+                    <img src={src}/>
+                </Form.Group>
                 <Form.Group className="title">
                     <Form.Control
                         type="text"
