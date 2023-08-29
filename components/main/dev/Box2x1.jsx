0580175eddb5c53af23e96b8965b0f9b445a4a44
seongho <damadu01@gmail.com>
2023-08-29 13:04:14 +0900

    box2x1 ui 완성

--- components/main/dev/Box2x1.jsx
+++ components/main/dev/Box2x1.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function Box2x1() {
   return (
-    <div>Box2x1</div>
+    <div className='box2'>
+        <div className='box2Title'>
+            <div>
+                <h2>Explore the information of</h2>
+                <h2>the Develop</h2>
+            </div>
+            <div className='sticker'>
+                <img src="/image/main/dev/interview.png" alt="interview" />
+                <img src="/image/main/dev/skill.png" alt="skill" />
+                <img src="/image/main/dev/develop.png" alt="develop" />
+            </div>
+        </div>
+        <div className='box2Img'>
+            <img src="/image/main/dev/computer.png" alt="computer" />
+        </div>
+    </div>
   )
 }
