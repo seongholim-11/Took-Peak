a6c68223f2bdd5512550092cd7f5e7fb3be6903c
seongho <damadu01@gmail.com>
2023-08-31 18:11:03 +0900

    게시판 댓글창 만들기

--- app/board/portfolio/[id]/page.js
+++ app/board/portfolio/[id]/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connectDB } from "@/util/database.js";
 import { ObjectId } from "mongodb";
-import Detail from "@/components/board/detail/PortDetail";
+import PortDetail from "@/components/board/detail/PortDetail";
 
 export default async function page(props) {
     const { id } = props.params;
@@ -9,5 +9,5 @@ export default async function page(props) {
     let db = (await connectDB).db("forum");
     let result = await db.collection("post").findOne({ _id: new ObjectId(id) });
 
-    return <Detail result={result} />;
+    return <PortDetail result={result} />;
 }
