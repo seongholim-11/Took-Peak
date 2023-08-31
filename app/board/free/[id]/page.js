2deacff9d732f0d6f44ef944cf144a0caedafe70
seongho <damadu01@gmail.com>
2023-08-31 18:11:03 +0900

    게시판 댓글창 만들기

--- app/board/free/[id]/page.js
+++ app/board/free/[id]/page.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import { connectDB } from "@/util/database.js";
+import { ObjectId } from "mongodb";
+import Detail from "@/components/board/detail/Detail";
 
-export default function page() {
-  return (
-    <div>page</div>
-  )
+export default async function page(props) {
+    const { id } = props.params;
+
+    let db = (await connectDB).db("forum");
+    let result = await db.collection("post").findOne({ _id: new ObjectId(id) });
+    result._id = result._id.toString()
+
+    return <Detail result={result} />;
 }
--- components/board/detail/Detail.jsx
+++ components/board/detail/Detail.jsx
@@ -4,18 +4,24 @@ import React from "react";
 import Link from "next/link";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+
+import { usePathname, useSearchParams, useParams } from "next/navigation";
+
 import { BiArrowBack } from "react-icons/bi";
 import { RxDotFilled } from "react-icons/rx";
 
-import "@/components/board/detail/portDetail.scss";
+import "@/components/board/detail/detail.scss";
 
 export default function Detail({ result }) {
+    let url = usePathname();
+    const parts = url.split("/"); // 문자열을 "/"로 분리하여 배열 생성
+    const category = parts[2]; // 카테고리 부분을 추출
 
     return (
         <Container>
             <div className="wrap">
                 <div className="prev">
-                    <Link href="/board/portfolio">
+                    <Link href={`/board/${category}`}>
                         <Button variant="outline-primary">
                             <BiArrowBack />
                         </Button>
@@ -33,10 +39,9 @@ export default function Detail({ result }) {
                         </div>
                         <div className="author">글쓴이: {result.author}</div>
                     </div>
-                    <div className="img">
-                        <img src={result.image} alt="image" />
+                    <div className="content">
+                        <pre>{result.content}</pre>
                     </div>
-                    <div className="content"><pre>{result.content}</pre></div>
                 </div>
             </div>
         </Container>
