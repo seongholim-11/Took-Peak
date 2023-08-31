bc090b243aabdea5b7ab82c59aeb38d3db2abee5
seongho <damadu01@gmail.com>
2023-08-31 15:45:35 +0900

    nav footer 애니메이션 추가

--- app/board/portfolio/[id]/page.js
+++ app/board/portfolio/[id]/page.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import { connectDB } from "@/util/database.js";
+import { ObjectId } from "mongodb";
+import Detail from "@/components/board/detail/PortDetail";
 
-export default function page() {
-  return (
-    <div>page</div>
-  )
+export default async function page(props) {
+    const { id } = props.params;
+
+    let db = (await connectDB).db("forum");
+    let result = await db.collection("post").findOne({ _id: new ObjectId(id) });
+
+    return <Detail result={result} />;
 }
