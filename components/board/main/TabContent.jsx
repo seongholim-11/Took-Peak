d43147ec743fc381bdfce9a6f81a02680909cebb
seongho <damadu01@gmail.com>
2023-09-07 15:22:06 +0900

    - 공지사항 상세 페이지 글 UI 반응형으로 글자 생략

--- components/board/main/TabContent.jsx
+++ components/board/main/TabContent.jsx
@@ -2,21 +2,25 @@
 import React from "react";
 import Link from "next/link";
 
+import TitleLength from "./TitleLength";
+
 // components/main/Board 컴포넌트에게 넘겨받는 누른 탭메뉴와 해당 탭메뉴 데이터
 export default function TabContent({ boardData, activeTab }) {
     return (
         <>
-        {/* 넘겨받은 데이터가 비어있으면 "업로드된 글이 없습니다." 출력 */}
+            {/* 넘겨받은 데이터가 비어있으면 "업로드된 글이 없습니다." 출력 */}
             {boardData.length > 0 ? (
                 <div className="mainBoardWrap">
                     {boardData.map((item, idx) => {
-                            let createdAt = item.createdAt.substr(0, 10);
-                            let title = item.title.substr(0, 20);
+                        let createdAt = item.createdAt.substr(0, 10);
                         return (
                             /* 글을 누르면 해당 글의 상세 페이지로 이동 */
-                            <Link href={`/board/${activeTab}/${item._id}`} key={idx}>
+                            <Link
+                                href={`/board/${activeTab}/${item._id}`}
+                                key={idx}
+                            >
                                 <div className="content">
-                                    <div className="title">{item.title.length > 25 ? (`${title}...`) : (item.title)}</div>
+                                    <TitleLength item={item} />
                                     <div className="authorCreate">
                                         {item.author} | {createdAt}
                                     </div>
---
+++ components/board/main/TitleLength.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+
+export default function TabContent({ item }) {
+    const [titleLength, setTitleLength] = useState(50); // 초기값
+    const [windowSize, setWindowSize] = useState(window.innerWidth);
+    const [retitle, setRetitle] = useState(""); // retitle을 상태로 변경
+    const newWindowWidth = window.innerWidth;
+
+    useEffect(() => {
+        // 화면 크기에 따라 제목 길이를 동적으로 변경
+        function handleResize() {
+            setWindowSize(newWindowWidth);
+            if (newWindowWidth > 992) {
+                setTitleLength(50);
+            } else if (newWindowWidth > 769 && newWindowWidth < 991) {
+                setTitleLength(20);
+            } else if (newWindowWidth < 768) {
+                setTitleLength(15);
+            }
+        }
+
+        // 화면 크기 변경 이벤트 리스너 등록
+        window.addEventListener("resize", handleResize);
+
+        // 컴포넌트 언마운트 시 리스너 제거
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, [newWindowWidth]); // 빈 배열을 전달하여 컴포넌트가 처음 마운트될 때만 이펙트를 실행
+
+    useEffect(() => {
+        setRetitle(item.title.substr(0, titleLength)); // retitle 상태 업데이트
+    }, [item.title, titleLength]);
+
+    return (
+        <>
+            <div className="title">
+                {item.title.length > titleLength ? `${retitle}...` : item.title}
+            </div>
+        </>
+    );
+}
