7d4ffe6b788694b82d7519a59735381ca67db22d
seongho <damadu01@gmail.com>
2023-09-13 13:04:55 +0900

    수정, 삭제 버튼 추가

--- components/board/detail/Back.jsx
+++ components/board/detail/Back.jsx
@@ -7,12 +7,21 @@ export default function Back() {
     function onBackClick() {
         router.back();
     }
+
+    // 글쓴이와 현재 로그인된 유저 비교
+    
     return (
-        <div className="prev">
+        <div className="ped">
             {/* 뒤로가기 버튼을 누르면 해당 글의 카테고리 페이지로 이동 */}
-            <Button variant="outline-primary" onClick={onBackClick}>
-                <BiArrowBack />
-            </Button>
+            <div className="p">
+                <Button variant="outline-primary" onClick={onBackClick}>
+                    <BiArrowBack />
+                </Button>
+            </div>
+            <div className="ed">
+                <Button variant="outline-primary">Edit</Button>
+                <Button variant="outline-danger">Delete</Button>
+            </div>
         </div>
     );
 }
