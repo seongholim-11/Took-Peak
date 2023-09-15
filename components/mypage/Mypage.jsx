b3054cf75e51e0917605c6ff5dcb0321472575f3
seongho <damadu01@gmail.com>
2023-09-15 17:36:05 +0900

    마이페이지 제작 전

--- components/mypage/Mypage.jsx
+++ components/mypage/Mypage.jsx
@@ -1,13 +1,21 @@
 "use client";
+
+// react
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+// 로그인, 로그아웃 기능을 위해 import
+import { signIn } from "next-auth/react";
+import { signOut } from "next-auth/react";
+// react-icons
 import { MdContactPage } from "react-icons/md";
 import { MdOutlineAppRegistration } from "react-icons/md";
 import { BiLogOut } from "react-icons/bi";
 import { BiLogIn } from "react-icons/bi";
-
-import { useState } from "react";
-import { useSession } from "next-auth/react";
 import { CgProfile } from "react-icons/cg";
+// react-bootstrap
 import Button from "react-bootstrap/Button";
+// scss
 import "./mypage.scss";
 
 export default function Mypage() {
@@ -21,24 +29,30 @@ export default function Mypage() {
                 <div className="buttonWrap" style={{ display: "block" }}>
                     {session.data ? (
                         <div className="buttons">
-                            <button>
-                                <MdContactPage />
-                                <p>my page</p>
-                            </button>
-                            <button>
+                            <Link href={'/mypage'}>
+                                <button>
+                                    <MdContactPage />
+                                    <p>my page</p>
+                                </button>
+                            </Link>
+                            <button
+                                onClick={() => {
+                                    signOut();
+                                }}
+                            >
                                 <BiLogOut />
                                 <p>logout</p>
                             </button>
                         </div>
                     ) : (
                         <div className="buttons">
-                            <button>
+                            <button
+                                onClick={() => {
+                                    signIn();
+                                }}
+                            >
                                 <BiLogIn />
                                 <p>login</p>
-                            </button>
-                            <button>
-                                <MdOutlineAppRegistration />
-                                <p>sign up</p>
                             </button>
                         </div>
                     )}
